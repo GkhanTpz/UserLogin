ec54e65658e4d06b5aa2770f5a3663683605efcc
Gökhan TOPUZ <gokhan061@msn.com>
2023-05-12 19:38:17 +0300

    Update user_login.c

--- user_login.c
+++ user_login.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "loginstatus.h" // girilen kullanıcı bilgisini kontrol etmek için oluşturulan kütüphane
+#include "loginstatus.h" // This library checks username and password entered by user
 
 
 int main()
@@ -15,16 +15,17 @@ int main()
         printf("%s","Enter password: ");
         scanf ("%d",&password);
 
-        if(strcmp(DEFAULT_USERNAME,username) == 0 && DEFAULT_PASSWORD == password) // giriş başarılı ise;
+        if(strcmp(DEFAULT_USERNAME,username) == 0 && DEFAULT_PASSWORD == password) // If username entered user is true
         {
-            userCheck(LOGIN_SUCCESS); // kullanıcı bilgileri doğru ise fonksiyona girişin başarılı olduğu bildirilir.
+            userCheck(LOGIN_SUCCESS); // This declarate the function that user info is true
             break;
         }
-        else if((strcmp(DEFAULT_USERNAME,username) != 0 && DEFAULT_PASSWORD != password) || (strcmp(DEFAULT_USERNAME,username) != 0 && DEFAULT_PASSWORD == password)) // kullanıcı adı ve şifre hatalı ise;
-            userCheck(LOGIN_FAILURE_BAD_CREDENTIALS); // kullanıcı bilgilerinin hatalı olduğunu fonksiyona bildirir.
+        // If username and password entered by user is false
+        else if((strcmp(DEFAULT_USERNAME,username) != 0 && DEFAULT_PASSWORD != password) || (strcmp(DEFAULT_USERNAME,username) != 0 && DEFAULT_PASSWORD == password))
+            userCheck(LOGIN_FAILURE_BAD_CREDENTIALS); // This declarate the function that user info is false
 
-        else if(strcmp(DEFAULT_USERNAME,username) == 0 && DEFAULT_PASSWORD != password) // girilen şifre hatalı ise;
-            userCheck(LOGIN_FAILURE_BAD_PASSWORD); // kullanıcı tarafından girilen şifresinin hatalı olduğunu fonksiyona bildirir.
+        else if(strcmp(DEFAULT_USERNAME,username) == 0 && DEFAULT_PASSWORD != password) // If password entered user is false
+            userCheck(LOGIN_FAILURE_BAD_PASSWORD); // This declarate the function that password entered by user is false
     }
 #else
     printf("loginstatus.h is not defined or not located");
